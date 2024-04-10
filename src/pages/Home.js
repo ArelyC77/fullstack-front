22aacd2db430b0c44d27fb0a9a557fe726398362
Arely Correa-Perez <arely.corre@hotmail.com>
2024-04-10 08:24:41 -0400

    Connected Frontend to my Backend and made a dynamic table

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+    //tells react that your component has to do smth after
+    //the render
+    const [users, setUsers]=useState([])
+    //this lures the user's information everytime the page loads
+    useEffect(()=> {
+        loadUsers();
+    },[]);
+    //axios handles information from JSON in backend
+    const loadUsers=async ()=> {
+        const result=await axios.get("http://localhost:8080/users")
+        setUsers(result.data) //this prints to the table
+        //console.log(result.data) this prints to the console
+    };
+
+
     return (
         <div className='container'>
             {/* py-4 is for padding */}
-            <div className='py-4<'>
+            <div className='py-4'>
                 <table className="table table-bordered shadow">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            {/* <td colspan="2">Larry the Bird</td> */}
-                            <td >Larry the Bird</td>
-                            <td>Hi</td>
-                            <td>@twitter</td>
-                        </tr>
+                        {/* This is for dynamic data in table */}
+                        {
+                            users.map((user,index)=>(
+                                <tr>
+                                {/* increases the index by 1 */}
+                                <th scope="row" key={index}>
+                                    {index + 1}
+                                </th>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button className='btn btn-primary mx-2'>View</button>
+                                    <button className='btn btn-outline-primary mx-2'>Edit</button>
+                                    <button className='btn btn-danger mx-2'>Delete</button>
+                                </td>
+                                </tr>
+                            ))
+                        }
+                        
                     </tbody>
                 </table>
 
