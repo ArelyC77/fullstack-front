b6583d11aec8d4540d31551d6070f2c089f25cda
Arely Correa-Perez <arely.corre@hotmail.com>
2024-04-19 13:22:51 -0400

    editted the button placement on the modal for delete button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -110,8 +110,12 @@ export default function Home() {
                                 <p>Are you sure you want to delete this user?</p>
                             </div>
                             <div className="modal-footer">
-                                <button type="button" className="btn btn-danger" onClick={handleConfirmDelete}>Delete</button>
+                            <div style={{ position: 'absolute', bottom: '8', left: '0', right:'80%'}}>
                                 <button type="button" className="btn btn-secondary" onClick={handleCancelDelete}>Cancel</button>
+                                </div>
+                                <div className="ml-auto">
+                                    <button type="button" className="btn btn-danger" onClick={handleConfirmDelete}>Delete</button>
+                                </div>
                             </div>
                         </div>
                     </div>
